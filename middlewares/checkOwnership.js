28d52bf12c479db78e72130f152ad0aa794f4acb
Ahmed-Elimam <aa.emam97@gmail.com>
2025-05-09 07:15:14 +0300

    feat: checking request ownership for security

---
+++ middlewares/checkOwnership.js
@@ -0,0 +1,22 @@
+const AppError = require("../utils/AppError")
+
+const checkOwnership = (req, res, next) => {
+    const loogedUserId = req.user.id
+    const userRole = req.user.role
+    const reqUserId = req.param.id
+
+    if(userRole == "admin"){
+        return next();
+    }
+    
+    if(loogedUserId == reqUserId){
+        return next();
+    }
+    else{
+        return next(new AppError("Not authorized", 403));
+    }
+}
+
+module.exports = {
+    checkOwnership
+}
