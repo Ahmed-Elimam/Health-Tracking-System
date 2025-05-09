ac0e9cb5804333ccfc012a825d04a79c112cecb3
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-09 17:45:19 +0300

    fix: correct user data generation by defining generateUserData as a constant and updating phone number generation to use Egyptian format

--- factories/userFactory.js
+++ factories/userFactory.js
@@ -1,14 +1,17 @@
 const { faker } = require("@faker-js/faker");
 const User = require("../models/User");
 
-generateUserData = (overrides = {}) => {
+const generateUserData = (overrides = {}) => {
     return {
         email: faker.internet.email(),
         password: faker.internet.password(),
         firstName: faker.name.firstName(),
         lastName: faker.name.lastName(),
         nationalId: faker.datatype.number({ min: 10000000000000, max: 99999999999999 }).toString(),
-        phone: faker.phone.number(),
+        phone: [
+            generateEgyptianPhoneNumber(),
+            generateEgyptianPhoneNumber(),
+        ],
         addEventListener:{
             street: faker.address.streetAddress(),
             city: faker.address.city(),
@@ -25,6 +28,13 @@ generateUserData = (overrides = {}) => {
         ...overrides, // allows you to overwrite any field (optional)
     };
 }
+
+const generateEgyptianPhoneNumber = () => {
+    const prefixes = ['+2011', '+2012', '+2015', '+2010'];
+    const prefix = faker.helpers.arrayElement(prefixes);
+    const rest = faker.string.numeric(8); // 8 random digits
+    return prefix + rest;
+  }
 exports.createUser = async (userData) => {
     const data = generateUserData(userData);
     const user = await User.create(data);
