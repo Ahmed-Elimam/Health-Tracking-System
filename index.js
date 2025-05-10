15309e06db710d63e667ffd91c1500b5d6852650
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-10 17:38:52 +0300

    refactor: remove unused mongoose connection and seedUsers import in index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 require("dotenv").config();
-const mongoose = require("./config/dbConnection");
-
+// require("./seeds/seedUsers");
 const app = express();
 const port = 7070;
 // mongoose.connect;
