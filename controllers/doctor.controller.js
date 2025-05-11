878de04bf13d6adbb0a5df5db98a69c40b51bc2e
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-11 09:07:46 +0300

    feat: refactor doctor controller and service to use catchAsync for error handling and implement role-based access control for doctor retrieval

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -1,47 +1,27 @@
 const DoctorService = require("../services/doctor.service");
-const AppError = require("../utils/AppError");
+const catchAsync = require("../utils/catchAsync");
 
-exports.createDoctor = async (req, res, next) => {
-    try {
-        const doctor = await DoctorService.createDoctor(req.body);
-        res.status(201).json(doctor);
-    } catch (error) {
-        next(new AppError(error.message, 500));
-    }
-};
+exports.createDoctor = catchAsync( async (req, res) => {
+    const doctor = await DoctorService.createDoctor(req.body);
+    res.status(201).json(doctor);
+});
 
-exports.getDoctors = async (req, res, next) => {
-    try {
-        const doctors = await DoctorService.getDoctors(req.query);
-        res.status(200).json(doctors);
-    } catch (error) {
-        next(new AppError(error.message, 500));
-    }
-};
+exports.getDoctors = catchAsync( async (req, res) => {
+    const doctors = await DoctorService.getDoctors(req.query,req.user);
+    res.status(200).json(doctors);
+});
 
-exports.getDoctor = async (req, res, next) => {
-    try {
-        const doctor = await DoctorService.getDoctor(req.params.id);
-        res.status(200).json(doctor);
-    } catch (error) {
-        next(new AppError(error.message, 500));
-    }
-};
+exports.getDoctor = catchAsync( async (req, res) => {
+    const doctor = await DoctorService.getDoctor(req.params.id);
+    res.status(200).json(doctor);
+});
 
-exports.updateDoctor = async (req, res, next) => {
-    try {
-        const doctor = await DoctorService.updateDoctor(req.params.id, req.body);
-        res.status(200).json(doctor);
-    } catch (error) {
-        next(new AppError(error.message, 500));
-    }
-};
+exports.updateDoctor = catchAsync( async (req, res) => {
+    const doctor = await DoctorService.updateDoctor(req.params.id, req.body);
+    res.status(200).json(doctor);
+});
 
-exports.deleteDoctor = async (req, res, next) => {
-    try {
-        const doctor = await DoctorService.deleteDoctor(req.params.id);
-        res.status(200).json(doctor);
-    } catch (error) {
-        next(new AppError(error.message, 500));
-    }
-};
+exports.deleteDoctor = catchAsync( async (req, res) => {
+    const doctor = await DoctorService.deleteDoctor(req.params.id);
+    res.status(200).json(doctor);
+});
--- services/doctor.service.js
+++ services/doctor.service.js
@@ -1,12 +1,34 @@
 const Doctor = require('../models/Doctor');
+const Specialization = require('../models/Specialization');
 const {parasQueryParams} = require('../utils/queryParser');
 
-exports.getDoctors = async (query) => {
+const doctorResource_Patient = (doctor) => {
+    return {Specialization: doctor.specialization,clinicAddress: doctor.clinicAddress, bio: doctor.bio, experience: doctor.experience,
+        user:{firstName: doctor.userId.firstName, lastName: doctor.userId.lastName, email: doctor.userId.email, phone: doctor.userId.phone,profileImage: doctor.userId.profileImage} 
+    };
+}
+exports.getDoctors = async (query, user) => {
     const {filters, sorts} = parasQueryParams(query);
-    return await Doctor.find(filters).sort(sorts).populate("userId");
+    if(user.role == "admin" || user.role == "super-admin") return await Doctor.find(filters).sort(sorts).populate("userId");
+    else if(user.role == "patient") {
+        const treatings = await Treating.find({patientId: user._id}).populate("doctorId");
+        const oldDoctors = [];
+        const currentDoctors = [];
+        for (let i = 0; i < treatings.length; i++) {
+            if(treatings[i].isActive == false) oldDoctors.push(doctorResource_Patient(treatings[i].doctorId));
+            else currentDoctors.push(doctorResource_Patient(treatings[i].doctorId));
+        }
+        return {oldDoctors, currentDoctors};
+    };
 }
 
-exports.getDoctor = async(doctorId) => {
+exports.getDoctor = async(doctorId,user) => {
+    if(user.role == "admin" || user.role == "super-admin") return await Doctor.findById(doctorId).populate("userId");
+    else if(user.role == "doctor" && user._id == doctorId) return await Doctor.findById(doctorId).populate("userId");
+    else if(user.role == "patient") {
+        const doctor = await Doctor.findById(doctorId).populate("userId");
+        return doctorResource_Patient(doctor);
+    }
     return await Doctor.findById(doctorId).populate("userId");
 }
 
