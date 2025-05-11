50d3445bdcd2d623c9507eed7da53cbcaa282d42
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-11 14:05:23 +0300

    feat: implement role-based access control for treating retrieval and management in treating controller and service

--- controllers/treating.controller.js
+++ controllers/treating.controller.js
@@ -1,24 +1,64 @@
 const treatingService = require("../services/treating.service");
 const catchAsync = require("../utils/catchAsync");
 
-exports.getTreatings = catchAsync( async (req, res) => {
-    const treatings = await treatingService.getTreatings(req.query,req.user);
-    res.status(200).json(treatings);
+exports.getTreatings = catchAsync( async (req, res,next) => {
+    if(req.user.role == "admin" || req.user.role == "super-admin"){
+        const treatings = await treatingService.getTreatings_Admin(req.query);
+        res.status(200).json(treatings);
+    }
+    else if(req.user.role == "doctor"){
+        const treatings = await treatingService.getTreatings_Doctor(req.query,req.user);
+        res.status(200).json(treatings);
+    }
+    else if(req.user.role == "patient"){
+        const treatings = await treatingService.getTreatings_Patient(req.query,req.user);
+        res.status(200).json(treatings);
+    }
+    else
+        next(new AppError("Not authorized", 403));
 });
 
-exports.getTreating = catchAsync( async (req, res) => {
-    const treating = await treatingService.getTreatings(req.params.id,req.user);
-    res.status(200).json(treating);
+exports.getTreating = catchAsync( async (req, res,next) => {
+    if(req.user.role == "admin" || req.user.role == "super-admin"){
+        const treating = await treatingService.getTreatings_Admin(req.params.id);
+        res.status(200).json(treating);
+    }
+    else if(req.user.role == "doctor" ){
+        const treating = await treatingService.getTreatings_Doctor(req.params.id,req.user);
+        res.status(200).json(treating);
+    }
+    else if (req.user.role == "patient"){
+        const treating = await treatingService.getTreatings_Patient(req.params.id,req.user);
+        res.status(200).json(treating);
+    }
+    else
+        next(new AppError("Not authorized", 403));
 });
 
-exports.createTreating = catchAsync( async (req, res) => {
-    const treating = await treatingService.createTreatings(req.body,req.user);
-    res.status(201).json(treating);
+exports.createTreating = catchAsync( async (req, res,next) => {
+    if(req.user.role == "admin" || req.user.role == "super-admin"){
+        const treating = await treatingService.createTreatings(req.body);
+        res.status(200).json(treating);
+    }
+    else if(req.user.role == "doctor"){
+        const treating = await treatingService.createTreatings(req.body,req.user);
+        res.status(200).json(treating);
+    }
+    else
+        next(new AppError("Not authorized", 403));
 });
 
-exports.updateTreating = catchAsync( async (req, res) => {
-    const treating = await treatingService.updateTreatings(req.params.id, req.body,req.user);
-    res.status(200).json(treating);
+exports.updateTreating = catchAsync( async (req, res, next) => {
+    if(req.user.role == "admin" || req.user.role == "super-admin"){
+        const treating = await treatingService.updateTreatings(req.params.id, req.body);
+        res.status(200).json(treating);
+    }
+    else if(req.user.role == "doctor"){
+        const treating = await treatingService.updateTreatings(req.params.id, req.body,req.user);
+        res.status(200).json(treating);
+    }
+    else
+        next(new AppError("Not authorized", 403));
 });
 
 exports.deleteTreating = catchAsync( async (req, res) => {
--- services/treating.service.js
+++ services/treating.service.js
@@ -1,82 +1,80 @@
 const Patient = require("../models/Patient");
 const Treating = require("../models/Treating");
-const AppError = require("../utils/AppError");
+const Doctor = require("../models/Doctor");
 const {parasQueryParams} = require('../utils/queryParser');
 
-exports.getTreatings = async (query, user) => {
+exports.getTreatings_Admin = async (query) => {
     const {filters, sorts} = parasQueryParams(query);
-    if(user.role == "admin" || user.role == "super-admin")
-        return await Treating.find(filters).sort(sorts);
-    else if(user.role == "patient") {
-        const treatings = await Treating.find({patientId: user._id,...filters}).sort(sorts).exec();
-        const oldTreatings = [];
-        const currentTreatings = [];
-        for (let i = 0; i < treatings.length; i++) {
-            if(treatings[i].isActive == false) oldTreatings.push(treatingResource_Patient(treatings[i]));
-            else currentTreatings.push(treatingResource_Patient(treatings[i]));
-        }
-        return {oldTreatings, currentTreatings};
-    }
-    else if (user.role == "doctor") {
-        const treatings = await Treating.find({doctorId: user._id,...filters}).sort(sorts).exec();
-        const oldTreatings = [];
-        const currentTreatings = [];
-        for (let i = 0; i < treatings.length; i++) {
-            if(treatings[i].isActive == false) oldTreatings.push(treatingResource_Doctor(treatings[i]));
-            else currentTreatings.push(treatingResource_Doctor(treatings[i]));
-        }
-        return {oldTreatings, currentTreatings};
-    }
     return await Treating.find(filters).sort(sorts);
 }
+exports.getTreatings_Doctor = async (query, user) => {
+    const {filters, sorts} = parasQueryParams(query);
+    const doctor = await Doctor.findById(user.id);
+    const treatings = await Treating.find({doctorId: doctor._id,...filters}).sort(sorts).exec();
+    const oldTreatings = [];
+    const currentTreatings = [];
+    for (let i = 0; i < treatings.length; i++) {
+        if(treatings[i].isActive == false) oldTreatings.push(treatings[i]);
+        else currentTreatings.push(treatings[i]);
+    }
+    return {oldTreatings, currentTreatings};
+}
+exports.getTreatings_Patient = async (query, user) => {
+    const {filters, sorts} = parasQueryParams(query);
+    const patient = await Patient.findById(user.id);
+    const treatings = await Treating.find({patientId: patient._id,...filters}).sort(sorts).exec();
+    const oldTreatings = [];
+    const currentTreatings = [];
+    for (let i = 0; i < treatings.length; i++) {
+        if(treatings[i].isActive == false) oldTreatings.push(treatings[i]);
+        else currentTreatings.push(treatings[i]);
+    }
+    return {oldTreatings, currentTreatings};
+}
 
-exports.getTreating = async (id, user) => {
-    if (user.role == "admin" || user.role == "super-admin")
-        return await Treating.findById(id).populate("patientId").populate("doctorId").exec();
-    else if(user.role == "patient") {
-        const patient = await Patient.findById(user._id).exec();
-        const treating = await Treating.findById(id).exec();
-        if(treating.patientId == patient._id) return treatingResource_Patient(treating);
-        else throw new Error("You are not allowed to access this treating");
-    }
-    else if(user.role == "doctor") {
-        const doctor = await Doctor.findById(user._id).exec();
-        const treating = await Treating.findById(id).exec();
-        if(treating.doctorId == doctor._id) return treatingResource_Doctor(treating);
-        return treatingResource_Doctor(treating);
-    }
+exports.getTreating_Admin = async (id) => {
     return await Treating.findById(id).populate("patientId").populate("doctorId").exec();
 }
-
-exports.createTreating = async (TreatingData,user)=>{
-    if(user.role == "admin" || user.role == 'super-admin') return await Treating.create(TreatingData);
-    else if(user.role == "patient"){
-        const patient = Patient.find({userId: user._id}).exec();
-        if(patient._id == TreatingData.patientId) return await Treating.create(TreatingData);
-        else  throw new AppError("You are not allowed to access this treating");
-    }
-    else if(user.role == "doctor"){
-        const doctor = Doctor.find({userId: user._id}).exec();
-        if(doctor._id == TreatingData.doctorId) return await Treating.create(TreatingData);
-        else throw new AppError("You are not allowed to access this treating");
-    }
+exports.getTreating_Doctor = async (id, user) => {
+    const doctor = await Doctor.findById(user.id);
+    const treating = await Treating.findById(id).exec();
+    if(treating.doctorId == doctor._id) return treating;
+    return treating;
+}
+exports.getTreating_Patient = async (id, user) => {
+    const patient = await Patient.findById(user.id);
+    const treating = await Treating.findById(id).exec();
+    if(treating.patientId == patient._id) return treating;
+    return treating;
 }
 
-exports.updateTreating = async (id, TreatingData, user) => {
-    if (user.role == "admin" || user.role == "super-admin")
-        return await Treating.findByIdAndUpdate(id, TreatingData, { new: true }).exec();
-    else if(user.role == "patient") {
-        const patient = await Patient.findById(user._id).exec();
-        const treating = await Treating.findById(id).exec();
-        if(treating.patientId == patient._id) return await Treating.findByIdAndUpdate(id, TreatingData, { new: true }).exec();
-        else throw new Error("You are not allowed to access this treating");
-    }
-    else if(user.role == "doctor") {
-        const doctor = await Doctor.findById(user._id).exec();
-        const treating = await Treating.findById(id).exec();
-        if(treating.doctorId == doctor._id) return await Treating.findByIdAndUpdate(id, TreatingData, { new: true }).exec();
-        else throw new Error("You are not allowed to access this treating");
-    }
+exports.createTreating_Admin = async (TreatingData) => {
+    return await Treating.create(TreatingData);
+}
+exports.createTreating_Doctor = async (TreatingData, user) => {
+    const doctor = await Doctor.findById(user.id);
+    TreatingData.doctorId = doctor._id;
+    return await Treating.create(TreatingData);
+}
+exports.createTreating_Patient = async (TreatingData, user) => {
+    const patient = await Patient.findById(user.id);
+    TreatingData.patientId = patient._id;
+    return await Treating.create(TreatingData);
+}
+exports.updateTreating_Admin = async (id, TreatingData) => {
+    return await Treating.findByIdAndUpdate(id, TreatingData, { new: true }).exec();
+}
+exports.updateTreating_Doctor = async (id, TreatingData, user) => {
+    const doctor = await Doctor.findById(user.id);
+    const treating = await Treating.findById(id).exec();
+    if(treating.doctorId == doctor._id) return await Treating.findByIdAndUpdate(id, TreatingData, { new: true }).exec();
+    else throw new Error("You are not allowed to access this treating");
+}
+exports.updateTreating_Patient = async (id, TreatingData, user) => {
+    const patient = await Patient.findById(user.id);
+    const treating = await Treating.findById(id).exec();
+    if(treating.patientId == patient._id) return await Treating.findByIdAndUpdate(id, TreatingData, { new: true }).exec();
+    else throw new Error("You are not allowed to access this treating");
 }
 
 exports.deleteTreating = async (id, user) => {
