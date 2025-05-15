522bb3b848ac8946c45784d8081d0dc7754fafb3
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-15 16:23:13 +0300

    feat: add validation middleware and schemas for login and registration

---
+++ middlewares/validateRequest.js
@@ -0,0 +1,20 @@
+const AppError = require('../utils/AppError');
+/**
+ * Middleware to validate request body against a Joi schema
+ * @param {Joi.Schema} schema - Joi schema to validate against
+ * @returns {Function} Express middleware function
+ */
+const validateRequest = (schema) => {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      // If validation fails, send a 400 response with the error message
+      next(new AppError(error.message, 400));
+    }
+    // Attach the validated value to the request object
+    req.body = value;
+    next();
+  };
+};
+
+module.exports = validateRequest;
