5fc4011cbcb82c85c27a00b2e3a28eb6c2e34de8
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-15 16:25:17 +0300

    fix: correct formatting and optimize patient and specialization count retrieval in seeding script

--- seeds/seedThreads_Checkups.js
+++ seeds/seedThreads_Checkups.js
@@ -1,5 +1,5 @@
 const {createThread} = require("../factories/threadFactory");
-const{createCheckup} = require("../factories/checkupFactory");
+const {createCheckup} = require("../factories/checkupFactory");
 const {createPrescription} = require("../factories/prescriptionFactory");
 const {createTreating} = require("../factories/treatingFactory");
 const Patient = require("../models/Patient");
@@ -8,20 +8,21 @@ const Specialization = require("../models/Specialization");
 const Doctor = require("../models/Doctor");
 const Thread = require("../models/Thread");
 
-const patientCount = await Patient.countDocuments();
-const specializationCount = await Specialization.countDocuments();
-const DoctorCount = await Doctor.countDocuments();
-const threadCount = await Thread.countDocuments();
 async function seed() {
+  await dbConnect();
+  const patientCount = await Patient.countDocuments();
+  const specializationCount = await Specialization.countDocuments();
+  const DoctorCount = await Doctor.countDocuments();
+  const threadCount = await Thread.countDocuments();
+  // console.log(patientCount, specializationCount, DoctorCount, threadCount);
   await dbConnect();
   for (let i = 0; i < 10; i++) {
     const thread = await createThread();
     const randomInt = Math.floor(Math.random() * 3) + 1;
-    const randomPatients = Math.floor(Math.random() * await Patient.countDocuments());
+    const randomPatients = Math.floor(Math.random() * patientCount) + 1;
     const patientDoc = await Patient.findOne().skip(randomPatients).lean();
-    const randomSpecialization = Math.floor(Math.random() * await Specialization.countDocuments()) + 1;
+    const randomSpecialization = Math.floor(Math.random() * specializationCount) + 1;
     const specializationDoc = await Specialization.findOne().skip(randomSpecialization).lean();
-    console.log(randomSpecialization,randomPatients );
     for(let j = 0; j < randomInt; j++) {
       const checkup = await createCheckup({threadId: thread._id, patient: patientDoc, specializationId: specializationDoc._id},patientCount,DoctorCount,threadCount,specializationCount);
       await createTreating({threadId: thread._id, patient: patientDoc, specializationId: specializationDoc._id,doctorId: checkup.doctorId},patientCount,DoctorCount);
